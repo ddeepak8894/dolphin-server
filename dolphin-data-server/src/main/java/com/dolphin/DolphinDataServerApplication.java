ca06cde0d2b88b42188e6ddbac59ab34968371df
deepak.dhormare <deepak.dhormare@coupa.com>
2023-10-14 12:40:40 +0530

    14/102023 latest jar uploaded

--- dolphin-data-server/src/main/java/com/dolphin/DolphinDataServerApplication.java
+++ dolphin-data-server/src/main/java/com/dolphin/DolphinDataServerApplication.java
@@ -3,7 +3,6 @@ package com.dolphin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.integration.annotation.IntegrationComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import lombok.extern.java.Log;
